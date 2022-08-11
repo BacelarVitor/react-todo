9c428af202377ca21ec9828cb40f33ee986647a5
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-08-11 20:01:24 -0300

    create header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from './Header';
+import styles from './App.module.css';
+import './global.css'
+
+function App() {
+
+  return (
+      <>
+        <Header />
+        <div className={styles.wrapper}>
+        </div>
+      </>
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+import appLogo from './assets/logo.svg';
+import styles from './Header.module.css'
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={appLogo} />
+        </header>
+    )
+}
