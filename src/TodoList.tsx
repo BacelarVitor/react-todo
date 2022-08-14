e3008953a7b0eefba61b1c069f94db79f62f66e4
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-08-14 12:18:34 -0300

    estrutura de task

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,3 +1,4 @@
+import { Task } from './Task'
 import styles from './TodoList.module.css'
 
 export function TodoList() {
@@ -7,14 +8,19 @@ export function TodoList() {
                 <p>Tarefas criadas <span>0</span></p>
                 <p>Concluídas <span>0</span></p>
             </div>
-            <div className={styles.empty}>
-                <span>
-                    Você ainda não tem tarefas cadastradas
-                </span>
-                <span>
-                    Crie tarefas e organize seus itens a fazer
-                </span>
-            </div> 
+            <div className={styles.tasks}>
+                <Task />
+            </div>
+            {/*
+                <div className={styles.empty}>
+                    <span>
+                        Você ainda não tem tarefas cadastradas
+                    </span>
+                    <span>
+                        Crie tarefas e organize seus itens a fazer
+                    </span>
+                </div>
+            */}
         </div>
     )
 }
