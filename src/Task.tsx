7d6eb6ba5ae226f89195a7dac0f8e18ded9ab2d8
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-08-16 09:12:21 -0300

    finaliza desafio ajustando icon do checkbox

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,4 +1,4 @@
-import { Trash } from 'phosphor-react'
+import { CheckCircle, Circle, Trash } from 'phosphor-react'
 import React from 'react'
 import { ITask } from './ITask'
 import styles from './Task.module.css'
@@ -21,8 +21,11 @@ export function Task({ task, onCheckTask, onDeleteTask }: TaskProps) {
     return (
         <div className={styles.wrapper}>
             <div className={styles.task}>
-                <input type="checkbox" name="task" checked={task.isDone} onChange={handleTaskStatusChange}/>
-                <label htmlFor="task">{task.label}</label>
+                <label>
+                    <input type="checkbox" name="task" checked={task.isDone} onChange={handleTaskStatusChange}/>
+                    {task.isDone ? <CheckCircle className={styles.checkIcon} size={24} /> : <Circle className={styles.checkIcon} size={24}/> }
+                    <span className={styles.labelText}> {task.label}</span>
+                </label>
             </div>
             <button onClick={handleDeleteTask}>
                 <Trash className={styles.trash} size={24} />
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -13,12 +13,22 @@ export function TodoList({ tasks, handleCheckTask, handleDeleteTask }: TodoListP
         <div className={styles.wrapper}>
             <div className={styles.info}>    
                 <p>Tarefas criadas <span>{tasks.length}</span></p>
-                <p>Concluídas <span>{tasks.filter(t => t.isDone)?.length + ' de ' + tasks.length}</span></p>
+                <p>
+                    Concluídas 
+                    <span>
+                        {tasks.filter(t => t.isDone)?.length + ' de ' + tasks.length}
+                    </span>
+                </p>
             </div>
             <div className={styles.tasks}>
                 {tasks.length ? 
                     tasks.map(task => {
-                        return <Task task={task} key={task.id} onDeleteTask={handleDeleteTask} onCheckTask={handleCheckTask} />
+                        return <Task 
+                                    task={task} 
+                                    key={task.id} 
+                                    onDeleteTask={handleDeleteTask} 
+                                    onCheckTask={handleCheckTask} 
+                                />
                     })
                 :
                     <div className={styles.empty}>
